b9874f46e52a177bc0850d83f92baa75b1f6abb2
ChiLi_Lin <904981663@163.com>
2017-05-22 22:33:35 +0800

    Set Player Bullets
    Add players bullets.Any bullets spawn in server,but only run in client
    independently,because it doesn't need to change position or rotation
    from server.

--- Assets/Scrpits/PlayerController.cs
+++ Assets/Scrpits/PlayerController.cs
@@ -3,21 +3,42 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     void Update()
     {
         if (!isLocalPlayer)
+        {
             return;
+        }
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
     }
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<MeshRenderer>().material.color = Color.blue;
     }
-
 }
