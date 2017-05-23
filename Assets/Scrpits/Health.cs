a88c8880091e5feaadcdbe1e91494ab247328a71
ChiLi_Lin <904981663@163.com>
2017-05-23 16:19:17 +0800

    Death And Respawning
    Use [ClientRpc] attribute,it will called from server,but excuted on the
    clients.

--- Assets/Scrpits/Health.cs
+++ Assets/Scrpits/Health.cs
@@ -3,11 +3,12 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
-public class Health : NetworkBehaviour {
+public class Health : NetworkBehaviour
+{
 
     public const int maxHealth = 100;
 
-    //同步客户端，挂钩函数。
+    //HP同步客户端，挂钩OnChangeHealth函数。
     [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
 
@@ -15,19 +16,31 @@ public class Health : NetworkBehaviour {
 
     public void TakeDamage(int amount)
     {
+        //只有服务器才能操作伤害。
         if (!isServer)
             return;
-        
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+
+            // called on the Server, but invoked on the Clients
+            RpcRespawn();
         }
     }
 
-    void OnChangeHealth (int health)
+    void OnChangeHealth(int currentHealth)
     {
-        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            transform.position = Vector3.zero;
+        }
     }
 }
