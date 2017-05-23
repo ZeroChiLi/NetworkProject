69ec08cfc1b21484edf141d3481cacba9f90515d
ChiLi_Lin <904981663@163.com>
2017-05-23 10:20:37 +0800

    Add HealthBar,But Not Synchronized
    Add Health Image,Health script.but it doesn't synchronized.so it may
    different in all clients.

---
+++ Assets/Scrpits/Billboard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
---
+++ Assets/Scrpits/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+}
